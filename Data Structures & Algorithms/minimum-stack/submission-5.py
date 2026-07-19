9cc5696a69f37113f5beda92880645cd0d856528
Rohit Kukreja <rohit.kukreja01@gmail.com>
2026-07-20 00:33:02 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,27 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            self.minStack.append(min(self.minStack[-1], val))
+        else:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.minStack.pop()
+        
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
+
+        
